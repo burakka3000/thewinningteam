0843be6792526061a65eba7283ae42ba6b54ef24
Hanyi Xu <hanyixu36@gmail.com>
2019-08-12 15:43:53 +0200

    order change

--- CTG/CTG/Order.py
+++ CTG/CTG/Order.py
@@ -2,30 +2,25 @@ import pandas as pd
 import numpy as np
 
 class Order:
-    def __init__(self, order_type, stock_name, stock_volumn):
+    def __init__(self, order_type, stock_name, stock_volume, portfolio, data):
         self.order_type = order_type
         self.stock_name = stock_name
-        self.stock_volumn = stock_volumn
-        self.status = status # processing/success/failed
+        self.stock_volume = stock_volume
+        self.portfolio = portfolio
+        self.data = data
 
-        # self.balance = balance # float containing balance on account in EUR
-        # self.stocks = pd.DataFrame(columns=['stock','count']) # dataframe containing the stocks and the number owned, first column is stock name, second is count
-        # self.owner = [] # depicts owner of portfolio
+        self.status = "" # processing/success/failed
 
     def __repr__(self):
-        return 'Portfolio of ' + self.owner.get_name
+        return 'Order of ' + self.owner.get_name
 
-    def get_actions(self):
-        # userinput
-        stock_name = input("")
-        number_of_stocks = input("")
-        buy_or_sell = input("")
-        if buy_or_sell = "sell":
-            cash_mark = -1
-        if buy_or_sell = "buy":
-                cash_mark = 1
-        else:
-            Exception('Not enough funds, should be checked before')
+
+    Portfolio.get_balance
+
+
+    def get_order_(self):
+        return
+
         stock_price = data.get_data = pd.DataFrame # get the price of the stock
         cashchange = stock_price * number_of_stocks * cash_mark
         return self.actions
