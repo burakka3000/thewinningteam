5df9e598276f9883acd9903aacd9e4ad46b5e1b0
Hanyi Xu <hanyixu36@gmail.com>
2019-08-12 16:54:30 +0200

    upate order

--- CTG/CTG/Order.py
+++ CTG/CTG/Order.py
@@ -9,30 +9,20 @@ class Order:
         self.portfolio = Portfolio
         self.data = data
 
-        self.status = "" # processing/success/failed
-
     def __repr__(self):
         return 'Order of ' + self.owner.get_name
 
     def balance_check(self):
-        self.cashflow = 
         if self.order_type == "sell":
-            if self.stock_volume <= self.portfolio.stock_overview[self.stock_name]:
+            self.stock_volume = - self.stock_volume
+            self.cashflow = - self.stock_volumne * self.data.get_summary('price')
+            if - self.stock_volume <= self.portfolio.stock_overview[self.stock_name]:
                 Exception('Not enough stocks, check the number of stock to sell')
         if self.order_type == "buy":
-            if self.stock_volumne * self.data.get_summary('price') >= self.portfolio.balance:
+            self.stock_volume = self.stock_volume
+            self.cashflow = - self.stock_volumne * self.data.get_summary('price')
+            if - self.cashflow >= self.portfolio.balance:
                 Exception('Not enough stocks, check the number of stock to sell')
 
-    self.portfolio.update_balance()
-    self.portfolio.update_stocks()
-
-
-    def get_order_(self):
-        return
-
-        stock_price = data.get_data = pd.DataFrame # get the price of the stock
-        cashchange = stock_price * number_of_stocks * cash_mark
-        return self.actions
-
-    def get_status(self):
-        return self.status
+        self.portfolio.update_balance(self.cashflow)
+        self.portfolio.update_stocks(self.stock_name, self.stock_volume)
