e383251ada1124d93bba4928b572f19089a7f915
hzhou7 <haozhou@deloitte.nl>
2019-08-12 16:14:36 +0200

    no message

---
+++ CTG/CTG/Order.py
@@ -0,0 +1,24 @@
+class Order:
+    def __init__(self,type, stock, volume, data, portfolio):
+        self.type = type
+        self.stock = stock
+        self.volume = volume
+        self.data = data
+        self.portfolio = portfolio
+
+    def check_possible(self):
+        if self.type == 'sell':
+            if self.volume> self.portfolio.get_stocks():
+                print ('not enough stocks')
+                return False
+        if (self.volume*self.data.get_summary())>self.portfolio.get_balance():
+            print('Insufficient funds')
+            return False
+        return True
+
+    def execute(self):
+        if self.check_possible():
+            self.portfolio.update_balance(self.volume*self.data.get_summary())
+            self.portfolio.update_stocks(stock_name=self.stock, number_of_stocks=self.volume)
+
+
