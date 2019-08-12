4c453e757543401cafbc22aeb0b5cc38ca6cbdbf
hzhou7 <haozhou@deloitte.nl>
2019-08-12 14:56:33 +0200

    add interface class

--- CTG/CTG/test_Portfolio.py
+++ CTG/CTG/test_Portfolio.py
@@ -1,25 +1,18 @@
 import CTG.CTG.Portfolio as pf
 
 TestPortfolio = pf.Portfolio(balance = 1000)
-
 print(TestPortfolio.get_balance())
-
 print(TestPortfolio.get_stocks())
 
 TestPortfolio.update_balance(-100)
-
 print(TestPortfolio.get_balance())
-
-print(TestPortfolio.get_stocks())
 
 TestPortfolio.update_stocks('AAPL',10)
-
-print(TestPortfolio.get_balance())
-
 print(TestPortfolio.get_stocks())
 
 TestPortfolio.update_stocks('AAPL',-5)
-
-print(TestPortfolio.get_balance())
-
 print(TestPortfolio.get_stocks())
+
+import CTG.CTG.Interface as inf
+
+broker = inf.Interface(10000)
