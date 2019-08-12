2f440eae730bb4dd964d0d04d0cf6c62bdd9cd86
Hanyi Xu <hanyixu36@gmail.com>
2019-08-12 16:08:23 +0200

    update order

--- CTG/CTG/Order.py
+++ CTG/CTG/Order.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 
 class Order:
-    def __init__(self, order_type, stock_name, stock_volume, portfolio, data):
+    def __init__(self, order_type, stock_name, stock_volume, Portfolio, data):
         self.order_type = order_type
         self.stock_name = stock_name
         self.stock_volume = stock_volume
-        self.portfolio = portfolio
+        self.portfolio = Portfolio
         self.data = data
 
         self.status = "" # processing/success/failed
@@ -14,8 +14,17 @@ class Order:
     def __repr__(self):
         return 'Order of ' + self.owner.get_name
 
+    def balance_check(self):
+        self.cashflow = 
+        if self.order_type == "sell":
+            if self.stock_volume <= self.portfolio.stock_overview[self.stock_name]:
+                Exception('Not enough stocks, check the number of stock to sell')
+        if self.order_type == "buy":
+            if self.stock_volumne * self.data.get_summary('price') >= self.portfolio.balance:
+                Exception('Not enough stocks, check the number of stock to sell')
 
-    Portfolio.get_balance
+    self.portfolio.update_balance()
+    self.portfolio.update_stocks()
 
 
     def get_order_(self):
