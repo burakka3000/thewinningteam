83712b1313c3cdb94221f3ede7facd1c4adede74
bualp <bualp@deloitte.nl>
2019-08-13 17:02:03 +0200

    Updated data.py to include try except case for pulling data

--- CTG/CTG/data.py
+++ CTG/CTG/data.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import requests
+from datetime import date
 
 
 class Data():
@@ -14,37 +15,45 @@ class Data():
 
     def get_data(self):
 
-        #the ticker symbols for the stocks in our stockmarket
-        stocks = ['AAPL', 'MSFT', 'WFC', 'JNJ', 'DIS']
-        dic_of_dfs = {}
+        today = date.today()
 
-        for stock in stocks: #for loop to fetch data for all five stocks in our stockmarket
+        try:
+            summary_df = pd.read_csv("stock-data-"+str(today).split("-")[2]+"-"+str(today).split("-")[1]+".csv", sep=",", index_col='Unnamed: 0'))
+            self._summary = summary_df
 
-            response = requests.get(
-                "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+stock+"&interval=5min&outputsize=full&apikey=9JIMZ4CS31GW2257")
-            # Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
-            # See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-            if response.status_code != 200:
-                raise ValueError("Could not retrieve data, code:", response.status_code)
+        except:
+            #the ticker symbols for the stocks in our stockmarket
+            stocks = ['AAPL', 'MSFT', 'WFC', 'JNJ', 'DIS']
+            dic_of_dfs = {}
 
-            # The service sends JSON data, we parse that into a Python datastructure
-            raw_data = response.json()
-            df = pd.DataFrame.from_dict(raw_data['Time Series (5min)']).T #this line puts the time series values of the stokc in a df
-            if df.isnull().values.any() == True:
-                df.fillna(method='ffill') #fill na values if there are any
+            for stock in stocks: #for loop to fetch data for all five stocks in our stockmarket
 
-            dic_of_dfs[stock] = df #put the dataframe for every stock in this dictionary for every iteration of the loop
+                response = requests.get(
+                    "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+stock+"&interval=5min&outputsize=full&apikey=9JIMZ4CS31GW2257")
+                # Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
+                # See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+                if response.status_code != 200:
+                    raise ValueError("Could not retrieve data, code:", response.status_code)
 
-        summary = {'AAPL' : pd.to_numeric(dic_of_dfs['AAPL']['4. close'][0]),
-                   'MSFT' : pd.to_numeric(dic_of_dfs['MSFT']['4. close'][0]),
-                   'WFC' : pd.to_numeric(dic_of_dfs['WFC']['4. close'][0]),
-                   'JNJ' : pd.to_numeric(dic_of_dfs['JNJ']['4. close'][0]),
-                   'DIS' : pd.to_numeric(dic_of_dfs['DIS']['4. close'][0])}
+                # The service sends JSON data, we parse that into a Python datastructure
+                raw_data = response.json()
+                df = pd.DataFrame.from_dict(raw_data['Time Series (5min)']).T #this line puts the time series values of the stokc in a df
+                if df.isnull().values.any() == True:
+                    df.fillna(method='ffill') #fill na values if there are any
 
-        summary_df = pd.DataFrame(list(summary.items()), columns=['Ticker', 'Price'])
+                dic_of_dfs[stock] = df #put the dataframe for every stock in this dictionary for every iteration of the loop
 
-        self._data = dic_of_dfs #make the dict private and accessible within the class (for other methods)
-        self._summary = summary_df
+            summary = {'AAPL' : pd.to_numeric(dic_of_dfs['AAPL']['4. close'][0]),
+                       'MSFT' : pd.to_numeric(dic_of_dfs['MSFT']['4. close'][0]),
+                       'WFC' : pd.to_numeric(dic_of_dfs['WFC']['4. close'][0]),
+                       'JNJ' : pd.to_numeric(dic_of_dfs['JNJ']['4. close'][0]),
+                       'DIS' : pd.to_numeric(dic_of_dfs['DIS']['4. close'][0])}
+
+            summary_df = pd.DataFrame(list(summary.items()), columns=['Ticker', 'Price'])
+            summary_df.to_csv("stock-data-"+str(today).split("-")[2]+"-"+str(today).split("-")[1]+".csv", sep=",")
+
+            self._data = dic_of_dfs #make the dict private and accessible within the class (for other methods)
+            self._summary = summary_df
 
 
     def summary(self):
