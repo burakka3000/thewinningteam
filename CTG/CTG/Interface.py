98181a5a09cc847316bbe56a962b4671b8118a51
hzhou7 <haozhou@deloitte.nl>
2019-08-13 09:52:54 +0200

    updated interface and combined portfolio

--- CTG/CTG/Interface.py
+++ CTG/CTG/Interface.py
@@ -6,9 +6,11 @@ class Interface:
     def __init__(self, initial_balance):
         self.Portfolio = pf.Portfolio(balance = initial_balance)
         self.Data = dt.Data()
+        print('initialising portfolio...')
 
         while True:
             run = input('Check stock price[S], check portfolio[P] or place order[O]? [exit] to exit')
+            # get data
             if run == 'S':
                 choice1 = input('[price/plot]?')
                 self.Data.get_data()
@@ -17,25 +19,21 @@ class Interface:
                 elif choice1 == 'plot':
                     self.Data.get_summary(type=choice1)
 
+            # check portfolio
             elif run == 'P':
-                choice2 = input('Check balance[B] or stock portfolio [P]?')
-                if choice2 == 'B':
-                    print('balance equals :'+str(self.Portfolio.get_balance()))
-                elif choice2 == 'P':
-                    print(self.Portfolio.get_stocks())
-                else:
-                    print("That's not a valid command")
-
+                print('Returning portfolio statistics')
+                self.Portfolio.get_summary()
+            # place order
             elif run == 'O':
                 print('please specify order')
                 order_type = input('what is the order type: [buy/sell]')
                 stock = input('input stock ticker: ')
                 volume = float(input('enter volume: '))
+
                 self.Data.get_data()
                 new_order = ord.Order(type = order_type, stock = stock, volume = volume, portfolio = self.Portfolio, data =self.Data)
                 new_order.execute()
-                print('balance equals :' + str(self.Portfolio.get_balance()))
-                print(self.Portfolio.get_stocks())
+                self.Portfolio.get_summary()
 
             elif run == 'exit':
                 break
